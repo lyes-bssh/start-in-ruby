36fd4f00ba9da32be6b3d403854aeaef83fd507c
lyes <lyesboussaha.dev@gmail.com>
2023-01-17 11:57:36 +0100

    Exo 10 modifié

--- exo_10.rb
+++ exo_10.rb
@@ -2,6 +2,7 @@ puts "Salut, dis-moi t'es né quand ?"
 birthday = 1996
 puts "Je suis né en #{birthday}"
 puts "Tu avais quel âge en 2017 ?"
-if birthday = 1996
-  puts "J'avais 21 ans en 2017"
+age = 2017 - 1996
+if birthday == 1996
+  puts "J'avais #{age} en 2017"
 end
